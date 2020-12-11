ff1185f487a9d858fff97c3bcd10b0333e214438
santoshapatil <santoshapatil31@gmail.com>
2020-12-11 11:03:49 +0530

    Made front end changes

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def main():
     st.title("Review Miner  ")
     #st.text("By Santosh A Patil")
     pages = ["Home","How to"]
-    page = st.sidebar.selectbox("Select Page",pages)
+    page = st.sidebar.selectbox("Select Page",pages,key="page")
 
     st.sidebar.title("Key Idea")
     st.sidebar.info(
@@ -56,20 +56,19 @@ def main():
     if page == "Home":
         #st.text("https://www.amazon.in/Brayden-Portable-Blender-Rechargeable-Transparent/dp/B07NS898HJ/ref=cm_cr_arp_d_product_top?ie=UTF8")
         marketplace = ["amazon.in","flipkart.in","swiggy.com","zomato.com","oyorooms.com","rottentomatoes.com","mynrta.com"]
-        c1,c2 = st.beta_columns((1,3))
+        c1,c2,c3 = st.beta_columns((1,3,1))
         with c1:
-            choice = st.selectbox("Select Marketplace",marketplace)
+            choice = st.selectbox("Select Marketplace",marketplace,key="marketplace")
         with c2:
             product_url = st.text_input("Enter The Product url [Eg:https://www.amazon.in/dp/B07JWV47JW]")
-
+        with c3:
+            st.button('Analyze Reviews',key="go")
 
 
 
     if choice == "amazon.in":
         st.subheader("Amazon.in")
-        product_url = st.text_input("Enter The Product url [Eg:https://www.amazon.in/dp/B07JWV47JW]")
-
-        if st.button('Analyze Reviews'):
+        if st.button('Analyze Reviews') == "go":
             if "amazon.in" in product_url:
                 with st.spinner('Crawling Amazon to get reviews'):
                     data=amz(product_url)
