14e8b7a9f6507a4bb6cfe3e9bcc26e975aa50ff9
santoshapatil <santoshapatil31@gmail.com>
2021-01-17 20:08:55 +0530

    Addeed pages how_to

--- pages/how_to.py
+++ pages/how_to.py
@@ -1,3 +1,17 @@
 import streamlit as st
-def how_to():
-	
+import codecs
+import streamlit.components.v1 as components
+import os
+def About():
+    
+
+    f=codecs.open(r"C:\Users\SANTOSH A PATIL\Documents\GitHub\Review_miner\pages\test.html")
+    bod= f.read()
+    # st.beta_container():
+    st.subheader("")
+    with st.beta_container():
+          components.html(bod,width=100,height=1000,scrolling=True)
+         
+    
+
+About()
