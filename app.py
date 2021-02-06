e369a03c41164111d1f278c3e48fc12dfde9fdbd
SANTOSH A PATIL <santoshapatil31@gmail.com>
2021-02-06 12:21:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,11 +51,12 @@ def feedback_form():
 #     """,
 #     unsafe_allow_html=True,
 # )   
-    st.write("Click this [link](https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAADgGeC9UQVVCTENWQ0ZRQ04zUlZVOEtKMDQ2TUtQMC4u)")
+    st.write("Click this [link](https://www.intmood.com/report.html)")
+    
     # with st. beta_expander(label="Click to give feedback",expanded=False):
     #                         feedback="""<iframe width="100%" height= "1000px" src= "https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAADgGeC9UQVVCTENWQ0ZRQ04zUlZVOEtKMDQ2TUtQMC4u&embed=true" frameborder= "0" marginwidth= "0" marginheight= "0" style= "border: none; max-width:100%; max-height:100vh" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>"""
     #                         st.markdown(feedback,unsafe_allow_html=True)
-
+# https://www.intmood.com/report.html
 def start_time():
     start=time.time()
     return start 
